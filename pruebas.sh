d7a110ca523e741feda3ddf7f920e234c08d6ff5
Bryan Paxtor <56366872+bryanpaxtor@users.noreply.github.com>
2026-04-08 19:12:47 -0600

    IP-002, feat implementacion de ngnix como getaway y configuracion del archivo compose

--- pruebas.sh
+++ pruebas.sh
@@ -6,40 +6,42 @@ AZUL='\033[0;34m'
 NC='\033[0m' # No Color
 
 
-
-# Prueba 1
-echo -e "\n${VERDE}[1/6] Probando: Check General${NC}"
-echo "Ejecutando: curl localhost:8080/check"
-curl -s localhost:8080/check
+# Prueba 1 
+echo -e "\n${VERDE}[1/6] Probando: user/api/v1/${NC}"
+echo "Ejecutando: curl -H 'x-api-key: test' -H 'x-origin: local' http://localhost:8080/user/api/v1/"
+curl -s -H "x-api-key: test" -H "x-origin: local" http://localhost:8080/user/api/v1/
 echo -e "\n"
 
 # Prueba 2
-echo -e "${VERDE}[2/6] Probando: Core API v1${NC}"
-echo "Ejecutando: curl localhost:8080/core/api/v1/"
-curl -s localhost:8080/core/api/v1/
+echo -e "\n${VERDE}[2/6] Probando: user/api/v2/${NC}"
+echo "Ejecutando: curl -H 'x-api-key: test' -H 'x-origin: local' http://localhost:8080/user/api/v2/"
+curl -s -H "x-api-key: test" -H "x-origin: local" http://localhost:8080/user/api/v2/
 echo -e "\n"
 
 # Prueba 3
-echo -e "${VERDE}[3/6] Probando: Auth API v1${NC}"
-echo "Ejecutando: curl localhost:8080/api/v1/auth/"
-curl -s localhost:8080/auth/api/v1/
+echo -e "\n${VERDE}[3/6] Probando: user/api/v1234${NC}"
+echo "Ejecutando: curl -H 'x-api-key: test' -H 'x-origin: local' http://localhost:8080/user/api/v1234"
+curl -s -H "x-api-key: test" -H "x-origin: local" http://localhost:8080/user/api/v1234
 echo -e "\n"
 
 # Prueba 4
-echo -e "${VERDE}[4/6] Probando: Ruta inexistente (404)${NC}"
-echo "Ejecutando: curl localhost:8080/desconocida"
-curl -s localhost:8080/desconocida
+echo -e "\n${VERDE}[4/6] Probando: core/api/v1/${NC}"
+echo "Ejecutando: curl -H 'x-api-key: test' -H 'x-origin: local' http://localhost:8080/core/api/v1/"
+curl -s -H "x-api-key: test" -H "x-origin: local" http://localhost:8080/core/api/v1/
 echo -e "\n"
 
 # Prueba 5
-echo -e "${VERDE}[5/6] Probando: Core con API Key y Origin${NC}"
-echo "Ejecutando: curl -H 'x-api-key: abc' -H 'x-origin: prueba' localhost:8080/core/api/v1/"
-curl -s -H "x-api-key: abc" -H "x-origin: prueba" localhost:8080/core/api/v1/
+echo -e "\n${VERDE}[5/6] Probando: core/api/v2/${NC}"
+echo "Ejecutando: curl -H 'x-api-key: test' -H 'x-origin: local' http://localhost:8080/core/api/v2/"
+curl -s -H "x-api-key: test" -H "x-origin: local" http://localhost:8080/core/api/v2/
 echo -e "\n"
 
 # Prueba 6
+echo -e "\n${VERDE}[6/6] Probando: Check General${NC}"
+echo "Ejecutando: curl http://localhost:8080/check"
+curl -s http://localhost:8080/check
+echo -e "\n"
 
-# Prueba 6
-echo -e "${VERDE}[6/6] Probando: Auth con API Key y Origin${NC}"
-echo "Ejecutando: curl -H 'x-api-key: abc' -H 'x-origin: prueba' localhost:8080/api/v1/auth/"
-curl -s -H "x-api-key: abc" -H "x-origin: prueba" localhost:8080/api/v1/auth/
+
+
+
